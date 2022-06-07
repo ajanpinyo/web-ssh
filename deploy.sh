c158ac8f4c2e7d17461b670f08ee35b895c2e7b8
Pinyo <48814199+ajanpinyo@users.noreply.github.com>
2022-06-07 09:33:10 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 arch=$1  # arm arm64 amd64
-tag_arch=treehouses/webssh-tags:$arch
+tag_arch=ajanpinyo/webssh-tags:$arch
 tag_time=$(date +%Y%m%d%H%M)
-tag_arch_time=treehouses/webssh-tags:$arch-$tag_time
+tag_arch_time=ajanpinyo/webssh-tags:$arch-$tag_time
 echo $tag_arch_time
 
 docker tag $tag_arch $tag_arch_time
